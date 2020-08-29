56c7f69042c8e638960c2aab626850eab135b527
Dom <dammikesaman@gmail.com>
2020-08-29 18:14:39 +1200

    fetching csv data completed

--- src/main/java/com/dammike/covid/covidtracker/services/CovidTrackerService.java
+++ src/main/java/com/dammike/covid/covidtracker/services/CovidTrackerService.java
@@ -1,13 +1,31 @@
 package com.dammike.covid.covidtracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 public class CovidTrackerService {
 
     private static final String FETCH_DATA = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-    public void fetchCovidData() {
+    @PostConstruct
+    public void fetchCovidData() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(URI
+                .create(FETCH_DATA))
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
     }
 }
