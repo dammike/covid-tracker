b79b88ee3f99b851bba7aeb077a78235605c0c49
Dom <dammikesaman@gmail.com>
2020-08-29 19:03:17 +1200

    added service testing for fetching all data from server

--- src/test/java/com/dammike/covid/covidtracker/CovidTrackerApplicationTests.java
+++ src/test/java/com/dammike/covid/covidtracker/CovidTrackerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.dammike.covid.covidtracker;
 
+import com.dammike.covid.covidtracker.services.CovidTrackerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.io.IOException;
 
 @SpringBootTest
 class CovidTrackerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testFetchingCSVDataFromServer() throws IOException, InterruptedException {
+		CovidTrackerService service = new CovidTrackerService();
+		Assert.notNull(service.getAllData(), "CSV data loaded successfully!");
 	}
 
 }
